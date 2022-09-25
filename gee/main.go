c13671296217bcf80b8a59b9805593e49ee497e1
boh5 <bohuang1994@gmail.com>
2022-09-25 19:12:22 +0800

    :sparkles: Implement base handler

--- gee/main.go
+++ gee/main.go
@@ -6,28 +6,36 @@ import (
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/", indexHandler)
-	http.HandleFunc("/hello", helloHandler)
-	log.Fatal(http.ListenAndServe(":9999", nil))
-}
+// Engine is the uni handler for all requests
+type Engine struct{}
 
-// indexHandler echoes r.URL.Path
-func indexHandler(w http.ResponseWriter, req *http.Request) {
-	_, err := fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-}
-
-// helloHandler echoes r.Header
-func helloHandler(w http.ResponseWriter, req *http.Request) {
-	for k, v := range req.Header {
-		_, err := fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	switch req.URL.Path {
+	case "/":
+		_, err := fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+	case "/hello":
+		for k, v := range req.Header {
+			_, err := fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+			if err != nil {
+				log.Fatal(err)
+				return
+			}
+		}
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		_, err := fmt.Fprintf(w, "404 NOT FOUND: %s\n", req.URL)
 		if err != nil {
 			log.Fatal(err)
 			return
 		}
 	}
+}
+
+func main() {
+	engine := new(Engine)
+	log.Fatal(http.ListenAndServe(":9999", engine))
 }
