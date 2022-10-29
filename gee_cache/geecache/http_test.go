6c5dcbd805cb8204f2d2b64d97cc8e2fad31fe21
boh5 <bohuang1994@gmail.com>
2022-10-29 15:21:56 +0800

    :white_check_mark: Add test for http

--- gee_cache/geecache/http_test.go
+++ gee_cache/geecache/http_test.go
@@ -1,7 +1,48 @@
 package geecache_test
 
-import "testing"
+import (
+	"fmt"
+	"geecache"
+	"log"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestName(t *testing.T) {
+	t.Parallel()
 
+	addr := "localhost:9999"
+	baseUrl := "http://" + addr + "/_geecache/scores/"
+
+	go func() {
+		geecache.NewGroup("scores", 2<<10, geecache.GetterFunc(
+			func(key string) ([]byte, error) {
+				log.Println("[SlowDB] search key", key)
+				if v, ok := db[key]; ok {
+					return []byte(v), nil
+				}
+				return nil, fmt.Errorf("%s not exist", key)
+			}))
+
+		peers := geecache.NewHTTPPool(addr)
+		log.Println("geecache is running at", addr)
+		log.Fatal(http.ListenAndServe(addr, peers))
+	}()
+
+	key := "Tom"
+	resp, err := http.Get(baseUrl + key)
+	require.NoError(t, err)
+	require.Equal(t, http.StatusOK, resp.StatusCode)
+
+	data := make([]byte, resp.ContentLength)
+	_, err = resp.Body.Read(data)
+	require.EqualError(t, err, "EOF")
+	require.Equal(t, db[key], string(data))
+
+	key = "NotExist"
+	resp, err = http.Get(baseUrl + key)
+	require.NoError(t, err)
+	require.Equal(t, http.StatusInternalServerError, resp.StatusCode)
 }
